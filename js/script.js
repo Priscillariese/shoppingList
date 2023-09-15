19c479c391b981ab9f6d3854ea9d7425e53bdd27
Priscilla Riesegit config --global user.email priscilla.riese@gmail.com <priscilla.riese@gmail.com>
2023-09-15 14:09:10 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -50,10 +50,15 @@ function displayItems() {
             listItem.innerHTML = `<input type="text" id="editItem" value="${item.text}">
                                   <button id="btnsalve" onclick="saveEdit(${item.id})">Speichern</button>`;
         } else {
-            listItem.innerHTML = `${item.text}
-     <button class="btnEdit" onclick="editItem(${item.id})"><i class="fa fa-pencil"></i></button>
-       <button class="btnRemove" onclick="removeItem(${item.id})"><i class="fa fa-trash"></i></button>`;
+            listItem.innerHTML = `
+    <div style="overflow:hidden;text-overFlow:ellipsis;max-width:270px">${item.text}</div>
+    <span style="width:110px">
+         <button class="btnEdit" onclick="editItem(${item.id})"><i class="fa fa-pencil"></i></button>
+       <button class="btnRemove" onclick="removeItem(${item.id})"><i class="fa fa-trash"></i></button>
+       </span> `;
+       
         }
+
         shoppingList.appendChild(listItem);
     }
 }
